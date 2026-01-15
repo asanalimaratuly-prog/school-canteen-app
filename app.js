99191d89fa8f416a7636ec4143c42186320f4b78
asanalimaratuly-prog <asanali.maratuly@gmail.com>
2026-01-15 10:13:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -19,15 +19,10 @@ import {
 const firebaseConfig = {
 
   apiKey: "AIzaSyD3SQTDEmr7g8r9VHWX5Q-h4Tfq2d0rRiE",
-
   authDomain: "ashana-ca8a3.firebaseapp.com",
-
   projectId: "ashana-ca8a3",
-
   storageBucket: "ashana-ca8a3.firebasestorage.app",
-
   messagingSenderId: "1004661503332",
-
   appId: "1:1004661503332:web:ba12c7e9d25144c3f07671"
 
 };
